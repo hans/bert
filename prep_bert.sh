58f52c99277d1db23525eb17d9ae1508f15828e8
Jon Gauthier <jon@gauthiers.net>
2018-11-19 11:53:06 -0500

    support using finetuned models; support py2

--- prep_bert.sh
+++ prep_bert.sh
@@ -1,8 +1,11 @@
 INPUT=~/projects/nn-decoding/data/stimuli_384sentences.txt
 #MODEL="uncased_L-12_H-768_A-12" # BERT-Base, uncased
-MODEL="uncased_L-24_H-1024_A-16" # BERT-Large, uncased
+#MODEL="uncased_L-24_H-1024_A-16" # BERT-Large, uncased
+MODEL="finetune.uncased_L-12_H-768_A-12.MRPC" # BERT-Base, finetuned on MRPC
 BASEDIR=`pwd`
 MODELDIR="${BASEDIR}/${MODEL}"
+#CKPT_NAME="bert_model" # for some reason the pretrained models have this name that's different than what the fine-tuning scripts output
+CKPT_NAME="model"
 JSONL="encodings.${MODEL}.jsonl"
 NPY="encodings.${MODEL}.npy"
 LAYER="-1"
@@ -10,8 +13,8 @@ LAYER="-1"
 # Save BERT features to jsonl file
 python extract_features.py --input_file="${INPUT}" --output_file="${JSONL}" \
     --vocab_file="${MODELDIR}/vocab.txt" --bert_config_file="${MODELDIR}/bert_config.json" \
-    --init_checkpoint="${MODELDIR}/bert_model.ckpt" \
-    --layers="-1" --max_seq_length=64 --batch_size=8
+    --init_checkpoint="${MODELDIR}/${CKPT_NAME}.ckpt" \
+    --layers="-1" --max_seq_length=64 --batch_size=8 || exit 1
 
 # Convert jsonl to common npy encoding
 python process.py -i "${JSONL}" -l $LAYER -o "${NPY}"
--- process.py
+++ process.py
@@ -2,19 +2,18 @@
 
 from argparse import ArgumentParser
 import json
-from pathlib import Path
 
 import numpy as np
 
 
 p = ArgumentParser()
-p.add_argument("-i", "--encodings_file", default="encodings.jsonl", type=Path)
+p.add_argument("-i", "--encodings_file", default="encodings.jsonl")
 p.add_argument("-o", "--out_file", default="encodings.npy")
 p.add_argument("-l", "--layer", type=int, default=-1)
 
 args = p.parse_args()
 
-with args.encodings_file.open("r") as encs_f:
+with open(args.encodings_file, "r") as encs_f:
   encodings = [json.loads(line) for line in encs_f if line.strip()]
 
 
