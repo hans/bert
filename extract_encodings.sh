716a69e52d3527476b96dffbbcbdf9d25ad4cb41
Jon Gauthier <jon@gauthiers.net>
2018-11-21 16:05:07 -0500

    rename a few files

--- extract_encodings.sh
+++ extract_encodings.sh
@@ -1,7 +1,7 @@
 INPUT=~/projects/nn-decoding/data/stimuli_384sentences.txt
 #MODEL="uncased_L-12_H-768_A-12" # BERT-Base, uncased
 #MODEL="uncased_L-24_H-1024_A-16" # BERT-Large, uncased
-MODEL="finetune.uncased_L-12_H-768_A-12.MRPC" # BERT-Base, finetuned on MRPC
+MODEL="finetune.uncased_L-12_H-768_A-12.WNLI" # BERT-Base, finetuned on MRPC
 BASEDIR=`pwd`
 MODELDIR="${BASEDIR}/${MODEL}"
 #CKPT_NAME="bert_model" # for some reason the pretrained models have this name that's different than what the fine-tuning scripts output
@@ -17,4 +17,4 @@ python extract_features.py --input_file="${INPUT}" --output_file="${JSONL}" \
     --layers="-1" --max_seq_length=64 --batch_size=8 || exit 1
 
 # Convert jsonl to common npy encoding
-python process.py -i "${JSONL}" -l $LAYER -o "${NPY}"
+python process_encodings.py -i "${JSONL}" -l $LAYER -o "${NPY}"
