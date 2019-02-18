14a11644c786b63a638980e73c1a1a24eccf1c79
Jon Gauthier <jon@gauthiers.net>
2019-02-18 17:08:36 -0500

    run_squad: allow --num_train_steps override

--- run_squad.py
+++ run_squad.py
@@ -95,6 +95,10 @@ flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
 flags.DEFINE_float("num_train_epochs", 3.0,
                    "Total number of training epochs to perform.")
 
+flags.DEFINE_integer("num_train_steps", 0,
+                     "Total number of training steps to perform. "
+                     "(Overrides --num_train_epochs if given.)")
+
 flags.DEFINE_float(
     "warmup_proportion", 0.1,
     "Proportion of training to perform linear learning rate warmup for. "
@@ -746,8 +750,11 @@ def make_train_input_fn(tokenizer):
   """
   train_examples = read_squad_examples(
       input_file=FLAGS.train_file, is_training=True)
-  num_train_steps = int(
-      len(train_examples) / FLAGS.train_batch_size * FLAGS.num_train_epochs)
+  if FLAGS.num_train_steps == 0:
+    num_train_steps = int(
+        len(train_examples) / FLAGS.train_batch_size * FLAGS.num_train_epochs)
+  else:
+    num_train_steps = int(FLAGS.num_train_steps)
   num_warmup_steps = int(num_train_steps * FLAGS.warmup_proportion)
 
   # Pre-shuffle the input to avoid having to make a very large shuffle
