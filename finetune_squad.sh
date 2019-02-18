28752fc38adab4aec01553dec98af0c9143c5fb1
Jon Gauthier <jon@gauthiers.net>
2019-02-18 17:49:56 -0500

    fix squad script

---
+++ finetune_squad.sh
@@ -0,0 +1,22 @@
+MODEL="uncased_L-12_H-768_A-12" # BERT-Base, uncased
+BASEDIR=`pwd`
+MODELDIR="${BASEDIR}/${MODEL}"
+SQUAD_DIR=/om/data/public/jgauthie/squad-2.0
+MAX_TRAIN_STEPS=5000
+OUTPUTDIR="${BASEDIR}/finetune-${MAX_TRAIN_STEPS}.${MODEL}.SQuAD"
+
+python run_squad.py --do_train=true --do_eval=true \
+    --train_file="${SQUAD_DIR}/train-v2.0.json" \
+    --predict_file="${SQUAD_DIR}/dev-v2.0.json" \
+    --data_dir="${SQUAD_DIR}" --vocab_file "${MODELDIR}/vocab.txt" \
+    --bert_config_file "${MODELDIR}/bert_config.json" \
+    --init_checkpoint "${MODELDIR}/bert_model.ckpt" \
+    --save_checkpoints_steps 50 \
+    --train_batch_size 12 --max_seq_length 384 --doc_stride 128 --learning_rate 3e-5 \
+    --num_train_steps $MAX_TRAIN_STEPS \
+    --output_dir "${OUTPUTDIR}" \
+    --version_2_with_negative=True
+
+# symlink relevant helper data
+ln -s "${MODELDIR}/bert_config.json" "${OUTPUTDIR}/bert_config.json"
+ln -s "${MODELDIR}/vocab.txt" "${OUTPUTDIR}/vocab.txt"
