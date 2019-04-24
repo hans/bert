489fadf2c7ca4846afb008cf70629dcd98b8e35f
Jon Gauthier <jon@gauthiers.net>
2019-04-24 13:14:37 -0400

    run_pretraining: keep all checkpoints; save summaries very often

--- run_pretraining.py
+++ run_pretraining.py
@@ -460,6 +460,8 @@ def main(_):
       master=FLAGS.master,
       model_dir=FLAGS.output_dir,
       save_checkpoints_steps=FLAGS.save_checkpoints_steps,
+      keep_checkpoint_max=None,
+      save_summary_steps=FLAGS.save_checkpoints_steps,
       tpu_config=tf.contrib.tpu.TPUConfig(
           iterations_per_loop=FLAGS.iterations_per_loop,
           num_shards=FLAGS.num_tpu_cores,
