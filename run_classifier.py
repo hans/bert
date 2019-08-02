10c05e588ca18514b6537f183aadfc1275993f42
Jon Gauthier <jon@gauthiers.net>
2019-08-02 17:24:47 -0400

    add 5-class SST data processor

--- run_classifier.py
+++ run_classifier.py
@@ -347,6 +347,46 @@ class SstProcessor(DataProcessor):
     return examples
 
 
+class Sst5Processor(DataProcessor):
+  """Processor for the SST-5 data set."""
+
+  def get_train_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+  def get_dev_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+  def get_test_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
+  def get_labels(self):
+    """See base class."""
+    return list(map(str, range(5)))
+
+  def _create_examples(self, lines, set_type):
+    """Creates examples for the training and dev sets."""
+    examples = []
+    for (i, line) in enumerate(lines):
+      if i == 0:
+        continue
+      guid = "%s-%s" % (set_type, i)
+      if set_type == "test":
+        text_a = tokenization.convert_to_unicode(line[1])
+        label = "0"
+      else:
+        text_a = tokenization.convert_to_unicode(line[0])
+        label = tokenization.convert_to_unicode(line[1])
+      examples.append(
+          InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+    return examples
+
+
 class MrpcProcessor(DataProcessor):
   """Processor for the MRPC data set (GLUE version)."""
 
@@ -1154,6 +1194,7 @@ def main(_):
       "mrpc": MrpcProcessor,
       "xnli": XnliProcessor,
       "sst": SstProcessor,
+      "sst5": Sst5Processor,
       "qqp": QqpProcessor,
       "qnli": QnliProcessor,
       "rte": RteProcessor,
